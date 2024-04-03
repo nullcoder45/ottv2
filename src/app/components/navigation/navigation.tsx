1f29a80028ecc6dc34ed474dcf5d635abb398380
Clavis7 <nullcoder45@gmail.com>
2024-04-03 07:39:13 +0530

    nav added
    some css changes

---
+++ src/app/components/navigation/navigation.tsx
@@ -0,0 +1,9 @@
+function Navigation() {
+    return (
+    <div>
+    <img className='w-2/5 lg:w-1/5' src='Group-14430.png'/>
+    </div>
+    );
+    }
+    
+    export default Navigation;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Carosuel from "./components/carosuel/carosuel";
 import Footer from "./components/footer/footer";
 import Ero from "./components/hero/hero";
+import Navigation from "./components/navigation/navigation";
 import Value from "./components/value/value";
 
 export default function Home() {
   return (
-   <div>
+   <div className="text-2xl bg-black text-white p-7 lg:p-10 lg:text-5xl">
+    <Navigation/>
     <Ero/>
     <Value/>
     <Carosuel/>
